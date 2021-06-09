2140646dc9ca2a9f8373546611fb0453987b4790
David Campion <>
2021-06-09 20:01:22 +0200

    tests streamlit

---
+++ tests_streamlit/modelisation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 27 19:19:02 2021
+
+@author: david
+"""
+@st.cache #stocke le resultat
+def generate_data():
+    x = 1
+    return x
+
+            
+
+
+
+
+model.fit
+dump(model, 'model.joblib')
+            
+            
+            
+après 
+load('model.joblib')
