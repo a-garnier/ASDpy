f03ba38a6f2ad125ccc00cadf400037f54a667cf
David Campion <>
2021-09-20 17:52:17 +0200

    v5 cnn

--- 5_dc_use_cnn3.py
+++ 5_dc_use_cnn3.py
@@ -32,32 +32,51 @@ model = load_model(model_folder)
 
 df_result = pd.DataFrame(columns=['file', 'score']) # df result  with files and scores
 indiceFile = 0
-
-list_machine = ['fan', 'pump', 'slider', 'ToyCar',  'ToyConveyor', 'valve']  
-list_machine = ['fan', 'ToyCar', 'ToyConveyor', 'pump',  'slider', 'valve']  # ok ?
+nbPredictionOK = 0
+# list_machine = ['fan', 'pump', 'slider', 'ToyCar',  'ToyConveyor', 'valve']  
+list_machine = ['ToyCar', 'ToyConveyor', 'slider', 'pump', 'fan', 'valve']  # ok test
 # browse test files
 wavfiles = [f for f in listdir(png_test_folder) if isfile(join(png_test_folder, f))] # 7730 pngs
 for nameFilePngTotest in wavfiles:
     if nameFilePngTotest[-4:] != '.png': # ignore non .png files
         continue
+    arrName = nameFilePngTotest.split("_") # normal_id_06_00000451_pump.png
+    classPrefixReal = arrName[0] # 'normal' or 'anomaly'
+    classNameReal = arrName[4][:-4] # 'pump' / [:-4] remove the '.png'
+    # print(className)
+    # if classPrefixReal == 'anomaly':
+    #     continue
     # print('test nameFilePngTotest: ', nameFilePngTotest)
     test_image = image.load_img(png_test_folder + nameFilePngTotest, target_size = image_size) # , target_size = (64, 64)
     img_array = image.img_to_array(test_image)
     img_array = np.expand_dims(test_image, axis = 0)
     prediction = model.predict(img_array)
-    print(nameFilePngTotest, prediction)
-    # anomaly_id_02_00000078_ToyCar.png [[1.0803374e-02 9.8868352e-01 2.1100255e-12 2.1557459e-12 5.1306013e-04 9.0587385e-11]]
-    # anomaly_id_04_00000046_slider.png [[8.1691342e-26 3.0578049e-14 2.0197277e-34 6.1783719e-33 1.0000000e+00 1.3286750e-25]]
-    # normal_id_06_00000451_pump.png [[8.7345477e-08 2.6160514e-02 4.6769547e-04 9.4642216e-01 2.6945930e-02 3.5615378e-06]]
+    # print(nameFilePngTotest, prediction)
+    # print(nameFilePngTotest, np.rint(prediction)) # [[0. 0. 0. 1. 0. 0.]]
+    # print(nameFilePngTotest, np.rint(prediction).argmax(axis = 1)) # 
+    indiceClassNamePredict = np.rint(prediction).argmax(axis = 1)[0]
+    classOK = False
+    predictionOK = False
+    #todo : test aussi que la somme du tableau = 1 (un seul 1 dans le tableau)
+    # if the prediction is right, sound is normal otherwise, it's anormal
+    if classNameReal == list_machine[indiceClassNamePredict]:
+        classOK = True
+    if classOK and classPrefixReal == 'normal' or not classOK and classPrefixReal == 'anomaly':
+        predictionOK = True
+        nbPredictionOK+= 1
+    # print(nameFilePngTotest, result) # [[0. 0. 0. 1. 0. 0.]]
     # score = prediction[0]
+    print(nameFilePngTotest, np.rint(prediction), indiceClassNamePredict, list_machine[indiceClassNamePredict], predictionOK)
     # df_result = df_result.append({'file': nameFilePngTotest, 'score': score}, ignore_index=True)
     indiceFile += 1
-    if indiceFile == 5:
+    if indiceFile == 18:
         break
-    
+    # normal if list_machine
 # df_result = df_result.sort_values(by = ['score'], ascending = False)
 # df_result.head(40)
 
+accuracy = nbPredictionOK / indiceFile
+print('accuracy: ', accuracy)
 # countCorrectPrediction = 0
 # for index, row in df_result.iterrows():
 #     # print("file %s : %.2f%%" % (row['file'], row['score']))
@@ -74,3 +93,23 @@ for nameFilePngTotest in wavfiles:
 #     print("File %s predict %s (score: %.2f%%)." % (row['file'], correctPrediction, 100 * row['score']))
 
 # print("%s: accuracy:  %.2f" % (machine, countCorrectPrediction / len(df_result)))
+
+# anomaly_id_02_00000078_ToyCar.png [[0. 1. 0. 0. 0. 0.]] 1 ToyConveyor True
+# anomaly_id_04_00000046_slider.png [[0. 0. 0. 0. 1. 0.]] 4 fan True
+# anomaly_id_02_00000097_slider.png [[0. 0. 0. 0. 1. 0.]] 4 fan True
+# normal_id_06_00000451_pump.png [[0. 0. 0. 1. 0. 0.]] 3 pump True
+# anomaly_id_00_00000119_pump.png [[0. 1. 0. 0. 0. 0.]] 1 ToyConveyor True
+# anomaly_id_02_00000254_fan.png [[0. 0. 0. 0. 1. 0.]] 4 fan False
+# normal_id_06_00000637_pump.png [[0. 0. 0. 1. 0. 0.]] 3 pump True
+# normal_id_04_00000546_fan.png [[0. 0. 0. 0. 1. 0.]] 4 fan True
+# normal_id_02_00000492_valve.png [[0. 0. 0. 0. 0. 1.]] 5 valve True
+# normal_id_02_00000347_valve.png [[0. 0. 0. 0. 0. 1.]] 5 valve True
+# anomaly_id_00_00000024_valve.png [[0. 1. 0. 0. 0. 0.]] 1 ToyConveyor True
+# anomaly_id_03_00000031_ToyCar.png [[1. 0. 0. 0. 0. 0.]] 0 ToyCar False
+# anomaly_id_00_00000118_pump.png [[0. 0. 0. 1. 0. 0.]] 3 pump False
+# anomaly_id_02_00000244_fan.png [[0. 0. 0. 0. 1. 0.]] 4 fan False
+# anomaly_id_04_00000087_valve.png [[0. 0. 0. 0. 0. 1.]] 5 valve False
+# anomaly_id_02_00000205_ToyCar.png [[1. 0. 0. 0. 0. 0.]] 0 ToyCar False
+# anomaly_id_02_00000232_ToyCar.png [[1. 0. 0. 0. 0. 0.]] 0 ToyCar False
+# anomaly_id_00_00000135_fan.png [[0. 0. 1. 0. 0. 0.]] 2 slider True
+# accuracy:  0.61
